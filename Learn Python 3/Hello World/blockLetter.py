7556b7b4ef595a478b2d7847dcbc4aca03cabed5
kayrenemarie@gmail.com <kayrenemarie@gmail.com>
2025-08-19 19:33:44 -0400

    Learn Python 3/Hello World/Block Letters

---
+++ Learn Python 3/Hello World/blockLetter.py
@@ -0,0 +1,10 @@
+# Zara Anise Jones
+# She is so smart and she is a princess.
+
+print("ZZZZZ    A  ")
+print("    Z   A A ")
+print("   Z   A   A")
+print("  Z    AAAAA")
+print(" Z     A   A")
+print("Z      A   A")
+print("ZZZZZ  A   A")
